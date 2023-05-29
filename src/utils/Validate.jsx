cf2902dff3dfe53d1b036185c0c173df49404691
EEstaniel <edwineestaniel93@gmail.com>
2023-05-29 16:24:47 -0700

    fix: error for split components

--- src/utils/Validate.jsx
+++ src/utils/Validate.jsx
@@ -5,14 +5,15 @@ function Validate(e) {
 
     // Check if input.value is empty
     if (e.value.trim() === '') {
-        e.style.borderColor = 'red';
+        e.style.borderColor = 'hsl(359, 39%, 58%)';
         e.parentElement.setAttribute('error-after', "Can't be blank");
         hasError = true;
-        if (e.parentElement.id === 'date') {
-            e.parentElement.classList.add('error-group')
+        if (e.id === 'month-input' || e.id === 'year-input') {
+            e.parentElement.parentElement.classList.add('error-group')
+            e.parentElement.parentElement.setAttribute('error-after', "Can't be blank");
 
-        } else if (e.parentElement.id === 'cvc') {
 
+        } else if (e.id === 'cvc-input') {
             e.parentElement.classList.add('error-cvc')
         } else {
             e.parentElement.classList.add('error');
@@ -21,57 +22,56 @@ function Validate(e) {
 
     } else {
         // Check if name has first-name and second-name
-        if (e.parentElement.id === 'name-label') {
-            const name = e.value.split(' ');
+        if (e.id === 'name-input') {
+            const name = e.value.trim().split(' ');
             if (name.length < 2) {
-                e.style.borderColor = 'red';
+                e.style.borderColor = 'hsl(359, 39%, 58%)';
                 e.parentElement.setAttribute('error-after', "Please enter your full name");
                 e.parentElement.classList.add('error');
             }
             if (e.validity.patternMismatch) {
-                e.style.borderColor = 'red';
+                e.style.borderColor = 'hsl(359, 39%, 58%)';
                 e.parentElement.setAttribute('error-after', "Wrong format, letters only");
                 e.parentElement.classList.add('error');
             }
-        } else if (e.parentElement.id === 'number-label') {
+        } else if (e.id === 'number-input') {
             // Check if card number is 16 digits
             if (e.value.replace(/\s/g, '').length < 16) {
-                e.style.borderColor = 'red';
+                e.style.borderColor = 'hsl(359, 39%, 58%)';
                 e.parentElement.setAttribute('error-after', "Please enter 16 digits");
                 e.parentElement.classList.add('error');
 
             }
             if (e.validity.patternMismatch) {
-                e.style.borderColor = 'red';
+                e.style.borderColor = 'hsl(359, 39%, 58%)';
                 e.parentElement.setAttribute('error-after', "Wrong format, numbers only");
                 e.parentElement.classList.add('error');
 
             }
-        } else if (e.parentElement.id === 'date') {
-            if (e.id === 'month') {
+        } else if (e.id === 'month-input' || e.id === 'year-input') {
+            if (e.id === 'month-input') {
                 if (parseInt(e.value) < 1 || parseInt(e.value) > 12) {
-                    e.style.borderColor = 'red';
-                    e.parentElement.setAttribute('error-after', "Wrong format, numbers only");
-                    e.parentElement.classList.add('error-group');
+                    e.style.borderColor = 'hsl(359, 39%, 58%)';
+                    e.parentElement.parentElement.setAttribute('error-after', "Wrong format, numbers only");
+                    e.parentElement.parentElement.classList.add('error-group');
 
                 }
             }
             if (e.validity.patternMismatch) {
-                e.style.borderColor = 'red';
-                e.parentElement.setAttribute('error-after', "Wrong format, numbers only");
-                e.parentElement.classList.add('error-group');
+                e.style.borderColor = 'hsl(359, 39%, 58%)';
+                e.parentElement.parentElement.setAttribute('error-after', "Wrong format, numbers only");
+                e.parentElement.parentElement.add('error-group');
 
             }
-        } else if (e.parentElement.id === 'cvc') {
-            if (e.value.length < 3) {
-                e.style.borderColor = 'red';
-                e.parentElement.setAttribute('error-after', "Please enter 3 digits");
-                e.parentElement.classList.add('error-cvc');
+        } else if (e.id === 'cvc-input') {
 
-            }
             if (e.validity.patternMismatch) {
-                e.style.borderColor = 'red';
+                e.style.borderColor = 'hsl(359, 39%, 58%)';
                 e.parentElement.setAttribute('error-after', "Wrong format, numbers only");
+                e.parentElement.classList.add('error-cvc');
+            } else if (e.value.length < 3) {
+                e.style.borderColor = 'hsl(359, 39%, 58%)';
+                e.parentElement.setAttribute('error-after', "Please enter 3 digits");
                 e.parentElement.classList.add('error-cvc');
 
             }
