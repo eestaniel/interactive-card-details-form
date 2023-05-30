35c67246062a9a86efb30216927169ba04a34149
EEstaniel <edwineestaniel93@gmail.com>
2023-05-29 17:17:02 -0700

    feat: added font

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './styles/fonts.css'
 import './styles/styles.css'
 import './styles/reset.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
