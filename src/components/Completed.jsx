8212b60412f394fa9b388d27d7ee4b33a8e8afc2
EEstaniel <edwineestaniel93@gmail.com>
2023-05-29 17:34:09 -0700

    fix: svg class names

--- src/components/Completed.jsx
+++ src/components/Completed.jsx
@@ -5,11 +5,11 @@ const Completed = () => {
         <div className="completed-container">
             <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="40" cy="40" r="40" fill="url(#a)"/>
-                <path d="M28 39.92 36.08 48l16-16" stroke="#fff" stroke-width="3"/>
+                <path d="M28 39.92 36.08 48l16-16" stroke="#fff" strokeWidth="3"/>
                 <defs>
                     <linearGradient id="a" x1="-23.014" y1="11.507" x2="0" y2="91.507" gradientUnits="userSpaceOnUse">
-                        <stop stop-color="#6348FE"/>
-                        <stop offset="1" stop-color="#610595"/>
+                        <stop stopColor="#6348FE"/>
+                        <stop offset="1" stopColor="#610595"/>
                     </linearGradient>
                 </defs>
             </svg>
